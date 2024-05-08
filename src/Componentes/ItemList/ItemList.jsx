7a4a3854d4149b139b91c01346f82245bdd15916
Zeus-3131 <queleimporta545gmail.com>
2024-05-08 11:28:33 -0500

    conexion exitosa con el backend renderizando los productos

--- src/Componentes/ItemList/ItemList.jsx
+++ src/Componentes/ItemList/ItemList.jsx
@@ -1,91 +1,19 @@
-// import Item from "../Item/Item";
-// import "./ItemList.css";
-// import axios from "axios";
-// import { useState, useEffect } from "react";
+import Item from "../Item/Item";
+import "./ItemList.css";
 
-
-// const ItemList = ({ products }) => {
-  
-  // const [product, setProducts] = useState([]);
-  // const [prev,setPrev]=useState(null);
-  // const [next,setNext]=useState(null);
-  // useEffect(() => {
-  //   axios("http://localhost:8080/api/products")
-  //     .then((res) => {
-  //       setProducts(res.data.response.docs);
-  //       setPrev(res.data.response.prevPage);
-  //       setNext(res.data.response.nextPage);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
-//   return (
-//     <div className="ordenarProductos">
-//       {products.map((product) => (
-//         <Item key={product._id} {...product} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ItemList;
-
-// import React, { useState, useEffect } from "react";
-// import Item from "../Item/Item";
-// import "./ItemList.css";
-// import axios from "axios";
-
-// const ItemList = ({ initialProducts }) => { // Cambia el nombre de la prop para evitar conflicto
-//   const [products, setProducts] = useState([]); // Estado local para almacenar productos
-
-//   useEffect(() => {
-//     // Llamada a la API para obtener los productos
-//     axios.get("http://localhost:8080/api/products")
-//       .then((res) => {
-//         // Actualizar el estado con los productos obtenidos de la API
-//         setProducts(res.data.response.docs);
-//         console.log(res.data.response.docs);
-
-//       })
-//       .catch((err) => {
-//         console.log("Error fetching products:", err);
-//       });
-//   }, []); // Ejecutar solo una vez al montar el componente
-
-//   return (
-//     <div className="ordenarProductos">
-//       {products.map((product) => (
-//         <Item key={product._id} {...product} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ItemList;
-
-
-const ItemList = () => { //{ initialProducts }
-  // const [products, setProducts] = useState(initialProducts);
-
-  // useEffect(() => {
-  //   // Llamada a la API para obtener los productos
-  //   axios.get("http://localhost:8080/api/products")
-  //     .then((res) => {
-  //       // Actualizar el estado con los productos obtenidos de la API
-  //       setProducts(res.data.response.docs);
-        
-  //     })
-  //     .catch((err) => {
-  //       console.log("Error fetching products:", err);
-  //     });
-  // }, []); // Ejecutar solo una vez al montar el componente
-
+const ItemList = ({ products }) => {
   return (
     <div className="ordenarProductos">
-      {products.map((product) => (
-        <Item key={product._id} {...product} />
-      ))}
+      {products ? (
+        products.map((product) => (
+          <Item key={product._id} {...product} />
+        ))
+      ) : (
+        <p>Cargando productos...</p>
+      )}
     </div>
   );
 };
+
+
 export default ItemList;
--- src/Componentes/ItemListContainer/ItemListContainer.jsx
+++ src/Componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,70 +1,49 @@
-import "./ItemListContainer.css";
-import { useState, useEffect } from "react";
-// import { db } from "../../services/config";
-// import {collection, getDocs, where, query} from "firebase/firestore"
-import ItemList from "../ItemList/ItemList";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
 import Navbar from "../Navbar/Navbar";
 import Aside from "../Aditamentos/Aside/Aside";
 import Footer from "../Footer/Footer";
-import { NavLink } from "react-router-dom";
+import ItemList from "../ItemList/ItemList";
+import "./ItemListContainer.css"
 
 const ItemListContainer = ({ mostrarComponentes = true }) => {
-  // const [productos, setProductos] = useState([]);
-  // const { idCategoria } = useParams();
+  const [products, setProducts] = useState([]);
 
-  // useEffect(()=>{
-  //   const misProductos = idCategoria ? query(collection(db, "Inventario"), where("idcat", "==", idCategoria)): collection(db, "Inventario");
-
-  //   getDocs(misProductos)
-  //    .then(res=>{
-  //     const nuevosProductos = res.docs.map(doc=>{
-  //       const data = doc.data();
-  //       return {id:doc.id, ...data}
-  //     })
-  //     setProductos(nuevosProductos);
-  //    })
-  //    .catch(error=> console.log("Error",error))
-  // },[idCategoria])
-
-  // const [products, setProducts] = useState(initialProducts);
-
-  // useEffect(() => {
-  //   // Llamada a la API para obtener los productos
-  //   axios.get("http://localhost:8080/api/products")
-  //     .then((res) => {
-  //       // Actualizar el estado con los productos obtenidos de la API
-  //       setProducts(res.data.response.docs);
-  //       console.log(res.data.response.docs);
-  //     })
-  //     .catch((err) => {
-  //       console.log("Error fetching products:", err);
-  //     });
-  // }, []); // Ejecutar solo una vez al montar el componente
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/products")
+      .then((res) => {
+        setProducts(res.data.response);
+      })
+      .catch((err) => {
+        console.log("Error fetching products:", err);
+      });
+  }, []);
 
   return (
     <>
       {mostrarComponentes && <Navbar />}
-      {mostrarComponentes && <div className="ContenedorNavegacionContainer">
-        <nav className="navProductosContainer">
-          <div className="col-12Container">
-            <NavLink className="enlacePContainer" to="/">
-              Inicio
-            </NavLink>
-            <span>
-              <i className="faContainer fa-angle-right" />
-            </span>
-            <span>Calzado especial</span>
-          </div>
-        </nav>
-      </div>}
+      {mostrarComponentes && (
+        <div className="ContenedorNavegacionContainer">
+          <nav className="navProductosContainer">
+            <div className="col-12Container">
+              <NavLink className="enlacePContainer" to="/">
+                Inicio
+              </NavLink>
+              <span>
+                <i className="faContainer fa-angle-right" />
+              </span>
+              <span>Calzado especial</span>
+            </div>
+          </nav>
+        </div>
+      )}
 
       <div className="contenedorDivDelListContainer">
         {mostrarComponentes && <Aside />}
-        <div className="contenedorProductosItemListContainer"> 
-          <h2 className="itemh2">mis productos</h2>
-          {/* <ItemList productos={productos} /> */}
-          {/* <ItemList initialProducts={products} /> */}
+        <div className="contenedorProductosItemListContainer">
+          <h2 className="itemh2">Mis productos</h2>
+          <ItemList products={products} /> {/* Pasar los productos como prop a ItemList */}
         </div>
       </div>
       {mostrarComponentes && <Footer />}
